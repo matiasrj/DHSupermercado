8304eee48544fb460543c6141d570fb4094bee1a
matiasrj <matias@inipop.com>
2022-03-21 08:03:40 -0300

    FIX use_env_variable en index model

--- database/models/index.js
+++ database/models/index.js
@@ -11,12 +11,8 @@ config= config[env];
 const db = {};
 
 let sequelize;
-config.use_env_variable=false
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
+
+sequelize = new Sequelize(config.database, config.username, config.password, config);
 
 fs
   .readdirSync(__dirname)
